feb9278be54d0d971a385b53ab8b2bbe710c0245
hgsgtk <kazuki@autify.com>
2022-09-16 19:01:14 +0900

    feat: openAndCloseBrowser

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
 import axios from 'axios';
 
-const url = 'http://localhost:9515';
-axios.get(url + "/status")
-    .then(function (response) {
+async function openAndCloseBrowser(url: string) {
+    let sessionId = '';
+
+    await axios.post(url + '/session', {
+        capabilities: {}
+    }, {
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    }).then(function (response) {
         console.log(`HTTP status code: ${response.status}`)
         console.log(JSON.parse(JSON.stringify(response.data)))
-    })
-    .catch(function (error) {
+    
+        sessionId = response.data.value.sessionId
+    }).catch(function (error) {
         console.log(error);
     });
+    
+    axios.delete(url + '/session/' + sessionId)
+        .then(function (response) {
+            console.log(`HTTP status code: ${response.status}`)
+            console.log(JSON.parse(JSON.stringify(response.data)))
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
-axios.post(url + '/session', {
-    capabilities: {}
-}, {
-    headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-    }
-}).then(function (response) {
-    console.log(`HTTP status code: ${response.status}`)
-    console.log(JSON.parse(JSON.stringify(response.data)))
-}).catch(function (error) {
-    console.log(error);
-});
+openAndCloseBrowser('http://localhost:9515')
