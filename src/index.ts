9e3c27b18918ea4f55784dbb910c6937f8a5e03b
hgsgtk <kazuki@autify.com>
2022-09-16 18:32:07 +0900

    fix: explicit logging

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const url = 'http://localhost:9515';
 axios.get(url + "/status")
     .then(function (response) {
-        console.log(response)
+        console.log(`HTTP status code: ${response.status}`)
+        console.log(JSON.parse(JSON.stringify(response.data)))
     })
     .catch(function (error) {
         console.log(error);
