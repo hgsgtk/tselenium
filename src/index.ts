bd085ade5004167fe692a93f26e7938caab941e9
hgsgtk <kazuki@autify.com>
2022-09-16 18:52:20 +0900

    feat: open a browser

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,16 @@ axios.get(url + "/status")
     .catch(function (error) {
         console.log(error);
     });
+
+axios.post(url + '/session', {
+    capabilities: {}
+}, {
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+}).then(function (response) {
+    console.log(`HTTP status code: ${response.status}`)
+    console.log(JSON.parse(JSON.stringify(response.data)))
+}).catch(function (error) {
+    console.log(error);
+});
